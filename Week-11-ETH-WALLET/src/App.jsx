c0ddc04788c9fc8ab696ec61637f0f1cedb0eea9
itanaysharma <thisistanaysharma@gmail.com>
2024-10-18 20:55:31 +0530

    Use of tanstack

--- Week-11-ETH-WALLET/src/App.jsx
+++ Week-11-ETH-WALLET/src/App.jsx
@@ -2,31 +2,36 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQueryClient,
+  useQuery,
+} from "@tanstack/react-query";
 // #https://jsonplaceholder.typicode.com/posts
+
+const queryClient = new QueryClient();
+async function getter() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/posts/");
+  const response = await data.json();
+  return response;
+}
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    // Create an async function inside useEffect
-    const fetchPosts = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
-      );
-      const json = await response.json();
-      setPosts(json);
-    };
-
-    // Call the async function
-    fetchPosts();
-  }, []);
-
   return (
-    <>
-      {posts.map((post) => (
-        <div key={post.id}> {post.title}</div>
-      ))}
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Posts></Posts>
+    </QueryClientProvider>
   );
+}
+function Posts() {
+  const queryClient = useQueryClient();
+  const query = useQuery({ queryKey: ["posts"], queryFn: getter });
+
+  if (query.isLoading) {
+    return <div>"Loading..."</div>;
+  }
+
+  return <div>{JSON.stringify(query.data)}</div>;
 }
 
 export default App;
